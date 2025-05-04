90f0b9faec0886e3333320e38ad171b0ac765589
Tepid Angler <tepidangler@protonmail.com>
2025-05-04 10:48:45 -0400

    Fixed Build System

---
+++ win32/rcVersion.h
@@ -0,0 +1,9 @@
+/*
+  rcVersion.h
+  This file is generated automatically by configure.js.
+*/
+
+#define LIBXML_MAJOR_VERSION 2
+#define LIBXML_MINOR_VERSION 14
+#define LIBXML_MICRO_VERSION 0
+#define LIBXML_DOTTED_VERSION "2.14.0"
